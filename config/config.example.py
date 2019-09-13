ba8eaf2e00bc6611ead09f86bf099dc25c5b8034
Will Szumski <will@stackhpc.com>
2019-09-13 22:02:55 +0100

    Update example config file to show that port strings are no longer supported

--- config/config.example.py
+++ config/config.example.py
@@ -4,11 +4,11 @@ flow_definitions = [
         "flows": [
             {
                 "flow": "vpn-inbound",
-                "src_port": "1194"
+                "src_port": 1194
             },
             {
                 "flow": "vpn-outbound",
-                "dst_port": "1194"
+                "dst_port": 1194
             },
         ],
     },
@@ -18,11 +18,11 @@ flow_definitions = [
         "flows": [
             {
                 "flow": "ceph-mon",
-                "dst_port": "6789",
+                "dst_port": 6789,
             },
             {
                 "flow": "ceph-mds",
-                "dst_port": "6800",
+                "dst_port": 6800,
             },
             # By default, Ceph OSD Daemons bind to the first available ports
             # on a Ceph Node beginning at port 6800
@@ -42,11 +42,11 @@ flow_definitions = [
         "flows": [
             {
                 "flow": "ceph-mon-inbound",
-                "src_port": "6789",
+                "src_port": 6789,
             },
             {
                 "flow": "ceph-mon-outbound",
-                "dst_port": "6789",
+                "dst_port": 6789,
             },
         ]
     },
@@ -56,11 +56,11 @@ flow_definitions = [
         "flows": [
             {
                 "flow": "ceph-mds-inbound",
-                "dst_port": "6800",
+                "dst_port": 6800,
             },
             {
                 "flow": "ceph-mds-outbound",
-                "src_port": "6800",
+                "src_port": 6800,
             },
         ]
     },
